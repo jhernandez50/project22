3511bbdb9141136caae8b0bf624e39025094cf78
Oliver Atom <j_hernandez50@hotmail.com>
2021-10-02 13:00:48 -0500

    second commit

---
+++ models/index.js
@@ -0,0 +1,44 @@
+const Customer = require('./Customer');
+const Itmember = require('./Itmember');
+const Ticket = require('./Ticket');
+
+// Ticket belongsTo Customer
+Ticket.belongsTo(Customer, {
+  foreignKey: 'customer_id'
+});
+
+// Ticket belongsTo ItMember
+Ticket.belongsTo(Itmemer, {
+    foreignKey: 'itmember_id'
+  });
+
+// Customer have many Tickets
+Customer.hasMany(Ticket, {
+  foreignKey: 'ticket_id'
+});
+
+// Itmemer have many Tickets
+Itmember.hasMany(Ticket, {
+    foreignKey: 'ticket_id'
+  });
+
+// Products belongToMany Tags (through ProductTag)
+/*
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
+// Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+*/
+
+module.exports = {
+  Customer,
+  Itmember,
+  Ticket,
+};
