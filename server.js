1213201db529c17639341417cbf27c495b59a6d0
Oliver Atom <j_hernandez50@hotmail.com>
2021-10-02 13:24:46 -0500

    Third commit

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
-const express = require('express');
-const routes = require('./routes');
 const sequelize = require('./config/connection');
+
+// TODO: Add a comment describing the functionality of this expression
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+// TODO: Add a comment describing the functionality of this object
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// TODO: Add a comment describing the functionality of this statement
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
 app.use(routes);
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
